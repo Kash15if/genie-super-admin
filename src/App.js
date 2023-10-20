ee2c591740908ae11b25debfe3e4fa8ce259db06
KASHIF AHMAD <43416961+Kash15if@users.noreply.github.com>
2023-10-20 23:48:27 +0530

    apdated and added routing

--- src/App.js
+++ src/App.js
@@ -1,89 +1,93 @@
 import logo from "./logo.svg";
 import "./App.css";
-import DynamicGrid from "./components/Grid";
-import React, { useState, useEffect } from "react";
-import PopupForm from "./components/Popup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import Pages from "./pages/Pages";
+import PageAssign from "./pages/PageAssign";
+import PageCreation from "./pages/PageCreation";
+import Users from "./pages/Users";
+
+// {"PageStructure":[
+//   {
+//     "compId": "c101",
+//     "CompNAme": "Card1",
+//     "CompType": "card",
+//     "sequence": 1
+//   }
+// ]}
 
 function App() {
-  const [selectedType, setSelectedType] = useState("");
-  const [layout, setLayout] = useState([]);
-  const [selectedIdForUpdate, setSelectedIdForUpdate] = useState();
+  // const [selectedType, setSelectedType] = useState("");
+  // const [layout, setLayout] = useState([]);
+  // const [selectedIdForUpdate, setSelectedIdForUpdate] = useState();
 
-  const [popupVisible, setPopupVisible] = useState(false);
+  // const [popupVisible, setPopupVisible] = useState(false);
 
-  const options = [
-    { value: "type1", label: "Type 1" },
-    { value: "type2", label: "Type 2" },
-    { value: "type3", label: "Type 3" },
-  ];
+  // const options = [
+  //   { value: "type1", label: "Type 1" },
+  //   { value: "type2", label: "Type 2" },
+  //   { value: "type3", label: "Type 3" },
+  // ];
 
-  const handleCreateNewGrid = (event) => {
-    // console.log(event.target.value); // Update the selected option when it changes
-    const newBoxId = (layout.length + 1).toString();
-    const newLayoutItem = {
-      i: newBoxId,
-      x: 0,
-      y: 2,
-      w: 2,
-      h: 2,
-      name: "Kashif",
-      type: event.target.value,
-      data: null,
-    };
-    setLayout([...layout, newLayoutItem]);
-  };
+  // const handleCreateNewGrid = (event) => {
+  //   // console.log(event.target.value); // Update the selected option when it changes
+  //   const newBoxId = (layout.length + 1).toString();
+  //   const newLayoutItem = {
+  //     i: newBoxId,
+  //     x: 0,
+  //     y: 2,
+  //     w: 2,
+  //     h: 2,
+  //     name: "Kashif",
+  //     type: event.target.value,
+  //     data: null,
+  //   };
+  //   setLayout([...layout, newLayoutItem]);
+  // };
 
-  const deleteOneGrid = (id) => {
-    const updatedLayout = layout.filter((item) => item.i !== id);
-    setLayout(updatedLayout);
-  };
+  // const deleteOneGrid = (id) => {
+  //   const updatedLayout = layout.filter((item) => item.i !== id);
+  //   setLayout(updatedLayout);
+  // };
 
-  const updateLayout = (newLayout) => {
-    setLayout(
-      newLayout.map((item) => {
-        const existingItem = layout.find((oldItem) => oldItem.i === item.i);
-        return {
-          ...item,
-          type: existingItem.type,
-          data: existingItem.data,
-          name: existingItem.name,
-        };
-      })
-    );
-  };
+  // const updateLayout = (newLayout) => {
+  //   console.log(
+  //     newLayout.map((item) => {
+  //       const existingItem = layout.find((oldItem) => oldItem.i === item.i);
+  //       return {
+  //         ...item,
+  //         type: existingItem.type,
+  //         data: existingItem.data,
+  //         name: existingItem.name,
+  //       };
+  //     })
+  //   );
+  //   setLayout(
+  //     newLayout.map((item) => {
+  //       const existingItem = layout.find((oldItem) => oldItem.i === item.i);
+  //       return {
+  //         ...item,
+  //         type: existingItem.type,
+  //         data: existingItem.data,
+  //         name: existingItem.name,
+  //       };
+  //     })
+  //   );
+  // };
 
   return (
-    <div className="App">
-      <div className="card flex justify-content-center">
-        <select value={selectedType} onChange={handleCreateNewGrid}>
-          <option key={0} value="">
-            Select One
-          </option>
-          {options.map((option, index) => (
-            <option key={index + 1} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
-      {layout && layout.length !== 0 && (
-        <DynamicGrid
-          layout={layout}
-          updateLayout={updateLayout}
-          deleteOneGrid={deleteOneGrid}
-          setPopupVisible={setPopupVisible}
-          setSelectedIdForUpdate={setSelectedIdForUpdate}
-        />
-      )}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Pages />} />
+        <Route path="/pages-structure/:pageId" element={<PageCreation />} />
+        <Route path="/pages-assign" element={<PageAssign />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
 
-      {selectedIdForUpdate && (
-        <PopupForm
-          visible={popupVisible}
-          setVisible={setPopupVisible}
-          componentDetails={selectedIdForUpdate}
-        />
-      )}
-    </div>
+    // </div>
   );
 }
 
